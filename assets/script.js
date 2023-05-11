1f31c551f966c3b7698fabad4c2160453cd76233
Flo700 <florian.martin700@gmail.com>
2023-05-11 16:25:04 +0200

    Ajout images et text

--- assets/script.js
+++ assets/script.js
@@ -20,13 +20,27 @@ const slides = [
 // Sélectionner les éléments HTML pertinents
 const leftArrow = document.getElementById("left-arrow");
 const rightArrow = document.getElementById("right-arrow");
+const bannerImg = document.getElementById("banner-img");
+const bannerDescription = document.getElementById("paragraph");
 
 // Ajouter un événement de click à la flèche gauche
 leftArrow.addEventListener("click", function (event) {
-	console.log ("click sur la flèche de gauche")
+    currentSlideIndex = (currentSlideIndex + - 1);
+    updateBanner()
 });
 
 // Ajouter un événement de click à la flèche droite
 rightArrow.addEventListener("click", function (event) {
-	console.log ("click sur la flèche de droite")
+    currentSlideIndex = (currentSlideIndex + 1);
+	updateBanner()
 });
+
+// Initialiser l'index de la diapositive actuelle à 0
+let currentSlideIndex = 0;
+
+// Fonction qui met à jour l'image et la description en fonction de l'index de la diapositive
+function updateBanner() {
+    const currentSlide = slides[currentSlideIndex];
+    bannerImg.src = "./assets/images/slideshow/" + currentSlide.image;
+    bannerDescription.innerHTML = currentSlide.tagLine;
+}
